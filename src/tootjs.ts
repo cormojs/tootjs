3271e12beafcf2ef38d6cd428c80cdc8c4f7b1a4
cormojs <cormorant2+github@gmail.com>
2018-02-07 07:06:15 +0900

    fix a bug

--- src/tootjs.ts
+++ src/tootjs.ts
@@ -42,7 +42,7 @@ export class Mastodon {
         let params = new URLSearchParams()
         for (let k in opts)
             params.set(k, opts[k]);
-        let q = opts.entries.length === 0 ? "" : "?"
+        let q = Object.keys(opts).length === 0  ? "" : "?"
         let f = await fetch(`https://${this.config.host}${this.config.api_base}${api}${q}${params.toString()}`,
                             { headers: new Headers({ "Authorization": `Bearer ${this.config.access_token}` })})
         return f.json()
